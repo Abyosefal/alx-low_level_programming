33566154fc6fb83bbbd026ab5c06c6a02a740071
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-08 11:11:20 -0700

    added script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints
+* Return: 0
+*/
+int main(void)
+{
+char a = 'A';
+int b = 120;
+long d = 145;
+long long e = 52;
+float c = 123.0f;
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long));
+printf("Size of a long long int: %lu byte(s)", sizeof(long long));
+printf("Size of a float: %lu byte(s)", sizeof(float));
+return (0);
+}
