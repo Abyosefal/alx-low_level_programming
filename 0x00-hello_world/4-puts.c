2366bac7578427c6e5e930259f20eea9a8ce20a3
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-08 07:38:25 -0700

    used betty function documenting for function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-/* @main prints a string using puts*/
+/**
+ *  @main prints a string using puts
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
