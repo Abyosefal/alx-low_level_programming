800091dbd2bcf62cea2fae1054877ebcdcdd1194
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-08 23:08:29 -0700

    anoter declaration fix to c file 1 code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int n;
+int digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int digit;
 digit = n % 10;
 if (n > 5)
 printf("Last digit of %d is %d and is greater than 5", n, digit);
