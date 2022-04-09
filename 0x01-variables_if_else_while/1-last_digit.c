5c12def444c6e62485ca24ff774adcaa6e9ca4d9
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-08 23:15:07 -0700

    exchanged order of if statments to treat equivalent equation first

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ int digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
-if (n > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-else if (n == 0)
+if (n == 0)
 printf("Last digit of %d is %d and is 0\n", n, digit);
+else if (n > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 return (0);
