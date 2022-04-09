506a722b8b3074e3c0dcae2c5aba773981492891
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-08 23:58:51 -0700

    c file 3 added code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 /**
- * main - prints
- * Return:0
- */
+* main - prints alphabets lower and upper
+* Return:0
+*/
 int main(void)
 {
-printf();
-return(0);
+unsigned char a;
+unsigned char b;
+int i;
+int j;
+a = 'a';
+b = 'A';
+for (i = 0; i < 26; i++)
+{
+putchar(a);
+a++;
+}
+for (j = 0; j < 26; j++)
+{
+putchar(b);
+b++;
+putchar('\n');
+}
+return (0);
 }
