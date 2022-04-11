a13a10291e4b5a3d6cabe1cf8fefeb1f19d518e8
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-11 06:16:26 -0700

    fixed script 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,9 @@
 int main(void)
 {
 int i;
-long int j = 987543210;
 for (i = 0; i < 10; i++)
 {
-putchar(j % 10);
-j = j % 10;
+putchar(i + '0');
 }
 putchar('\n');
 return (0);
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints all hex char
+ * Return: 0
+ */
+int void main()
+{
+
+}
