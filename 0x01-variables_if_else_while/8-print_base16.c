286a351b51d1945884e7590fec363f7c9b5766fc
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-11 06:24:37 -0700

    a fix to script 8, two loops to print hex with putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 /**
- * main - prints all hex char
- * Return: 0
- */
-int void main()
+* main - prints hex numbers and letters using putchar
+* Return:0
+*/
+int main(void)
 {
-
+int i;
+unsigned char j;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+for (j = 'a'; j < 'g'; j++)
+putchar (j + '0');
+putchar('\n');
+return (0);
 }
