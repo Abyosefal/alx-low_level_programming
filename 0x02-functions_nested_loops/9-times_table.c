f8a4842771809fb9804ccf3a2fbd213190e2fb65
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 07:13:12 -0700

    added code to file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,26 @@
-#include <stdio.h>
+#include "_putchar.h"
 /**
-* main - outputs
-* Return:0
+* times_table - counts every minute of the day and prints it
+* Return: void
 */
-int main(void)
+void times_table(void)
 {
-return (0);
+int i, j, num;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			num = i * j;
+			if (num < 10)
+				putchar(num + '0');
+			else
+			{
+				putchar((num / 10) + '0');
+				putchar((num % 10) + '0');
+			}
+		putchar(',');
+		putchar(' ');
+		}
+	putchar('\n');
+	}
 }
