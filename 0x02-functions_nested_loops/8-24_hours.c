7791daeca3780b0beafa5fe3de9a8db27f278730
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 06:47:22 -0700

    fixed betty-style error and checked code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,27 +5,27 @@
 */
 void jack_bauer(void)
 {
-	int m0, m1, h0, h1;
-        for (h0 = 0; h0 <= 2; h0++)
-        {
-                for (h1 = 0; h1 <= 9; h1++)
-                {
-                        for (m0 = 0; m0 < 6; m0++)
-                        {
-                                for (m1 = 0; m1 <= 9; m1++)
-                                {
-                                        _putchar(h0 + '0');
-                                        _putchar(h1 + '0');
-                                        _putchar(':');
-                                        _putchar(m0 + '0');
-                                        _putchar(m1 + '0');
-                                        _putchar('\n');
-                                      if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
-			             	break;
-                                }
-                        }
-                        if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
-			   break;
-                }
-        }
+int m0, m1, h0, h1;
+	for (h0 = 0; h0 <= 2; h0++)
+	{
+		for (h1 = 0; h1 <= 9; h1++)
+		{
+			for (m0 = 0; m0 < 6; m0++)
+			{
+				for (m1 = 0; m1 <= 9; m1++)
+				{
+					_putchar(h0 + '0');
+					_putchar(h1 + '0');
+					_putchar(':');
+					_putchar(m0 + '0');
+					_putchar(m1 + '0');
+					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+					break;
+				}
+			}
+			if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+			break;
+		}
+	}
 }
