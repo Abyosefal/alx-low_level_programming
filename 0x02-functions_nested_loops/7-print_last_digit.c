900a4c2e545912f8e7b0eeae24c5d42343f2ca9c
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 02:59:35 -0700

    temp fix to INT_MIN test error, tmp fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "_putchar.h"
-#include <stdio.h>
 #include <limits.h>
 /**
 * print_last_digit - print last digit of a number
@@ -11,8 +10,9 @@ int print_last_digit(int n)
 int last;
 	if (n < 0)
 		n = -1 * n;
+	if (n == INT_MIN)
+		n = 2147483648;
 	 last = n % 10;
 	_putchar(last + '0');
-	printf("%d\n", INT_MIN);
 	return (last);
 }
