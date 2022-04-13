1dace0802e1edd865653672f4b594a43ea869567
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-12 22:48:03 -0700

    added to c file 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+#include "_putchar.h"
 /**
-* main - outputs
+* print_alphabet_x10 - prints the alphabet 10 times
 * Return:0
 */
-int main(void)
+void print_alphabet_x10(void)
 {
-return (0);
+unsigned char a = 'a';
+int i, j;
+for (j = 0; j < 10; j++)
+{
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(a);
+		a++;
+	}
+}
+_putchar('\n');
 }
