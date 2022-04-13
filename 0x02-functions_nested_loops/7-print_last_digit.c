a386ef8fa00b6a5697fdff3b6a62fc6e31ee5d43
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 03:01:56 -0700

    temp fix to INT_MIN test error,  fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,11 @@
 * @n: automatic int var to take input
 * Return: ;ast digit of the number
 */
-int print_last_digit(long int n)
+int print_last_digit(unsigned long int n)
 {
 int last;
 	if (n < 0)
 		n = -1 * n;
-	if (n == INT_MIN)
-		n = 2147483648;
 	 last = n % 10;
 	_putchar(last + '0');
 	return (last);
