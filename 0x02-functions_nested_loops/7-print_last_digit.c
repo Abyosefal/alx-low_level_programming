3929f4d0873b26272d61045f6dc2f577bd560de2
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 04:24:28 -0700

    fix for 7, test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,10 @@
 int print_last_digit(int n)
 {
 int last;
-if (n < 0)
-	n = (-1 * n);
-last = n % 10;
+long int m = n;
+if (m < 0)
+	m = (-1 * m);
+last = m % 10;
 _putchar(last + '0');
 return (last);
 }
