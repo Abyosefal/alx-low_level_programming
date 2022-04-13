a318893f57f3a71a92056f97f0c4b053f69adf39
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 06:01:24 -0700

    fix to code 8, code change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,22 +6,26 @@
 void jack_bauer(void)
 {
 int m0, m1, h0, h1;
-	for (m0 = 0; m0 <= 2; m0++)
+	for (h0 = 0; h0 <= 2; h0++)
 	{
-		for (m1 = 0; m1 < 4; m1++)
+		for (h1 = 0; h1 < 4; h1++)
 		{
-			for (h0 = 0; h0 < 6; h0++)
+			for (m0 = 0; m0 < 6; m0++)
 			{
-				for (h1 = 0; h1 <= 9; h1++)
+				for (m1 = 0; m1 <= 9; m1++)
 				{
-					_putchar(m0 + '0');
-					_putchar(m1 + '0');
-					_putchar(':');
 					_putchar(h0 + '0');
 					_putchar(h1 + '0');
+					_putchar(':');
+					_putchar(m0 + '0');
+					_putchar(m1 + '0');
 					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+						break;
 				}
 			}
+			if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+				break;
 		}
 	}
 }
