7affaf568347338096fdffdddcf59d99ad310770
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-13 05:10:18 -0700

    fix 24 hr 0utput

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@
 void jack_bauer(void)
 {
 int m0, m1, h0, h1;
-	for (m0 = 0; m0 < 6; m0++)
+	for (m0 = 0; m0 <= 2; m0++)
 	{
-		for (m1 = 0; m1 <= 9; m1++)
+		for (m1 = 0; m1 < 4; m1++)
 		{
 			for (h0 = 0; h0 < 6; h0++)
 			{
