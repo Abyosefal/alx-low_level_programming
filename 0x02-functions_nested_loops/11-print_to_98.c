73a9a90eb2c5f7da58e370d4e99820edcad49382
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-14 01:03:00 -0700

    fix to code 11, handle multi digit output with _putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,41 @@ void print_to_98(int a)
 {
 	int i;
 
-	if ((a <=98 && a >= 0) || a < 0)
+	if ((a <= 98 && a >= 0) || a < 0)
 	{
 		for (i = a; i < 99; i++)
 		{
+			if (i < 10)
+			{
 			_putchar(i + '0');
 			_putchar(',');
 			_putchar(' ');
+			}
+			else if (i >= 10 && i < 100)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i >= 100 && i < 1000)
+			{
+				_putchar(i / 100 + '0');
+				_putchar((i % 100) / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else if (a > 98)
 	{
 		for (i = a; i < 99; i--)
-                {
-                        _putchar(i + '0');
-                        _putchar(',');
-                        _putchar(' ');
-                }
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	_putchar('\n');
 }
