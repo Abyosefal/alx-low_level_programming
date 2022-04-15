4fb833981bde4c8c694b9a39b7b10e65473a6d13
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-15 02:11:34 -0700

    added code 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks for given int and returns 1 if is between 1 and 9
+ * @c: input int
+ * Return: 1 if is digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c < 10)
+		return (1);
+	else
+		return (0);
+}
