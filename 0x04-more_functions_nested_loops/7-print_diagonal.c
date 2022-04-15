fb5688b87d77949619eb8de406ec6d6b7926130c
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-15 12:05:55 -0700

    added code to file 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal line on given length
+ * @n: given length of time
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i-1; j++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
