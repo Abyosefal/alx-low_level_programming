50d316bfbbd23d1a800dbc261e1a09dfd185299c
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-18 12:31:51 -0700

    initial code added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,21 @@
+#include "main.h"
+/**
+* print_rev - writes a string to stdout in reverse order
+* @s: string passed to the function to be printed
+* Return: nothing
+*/
+void print_rev(char *s)
+{
+	int i, j;
+	char arr[];
 
-
+	i = 0;
+	while (str[i] != '\0')
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	for (j = i; j >= 0; j--)
+		_putchar(arr[j]);
+	_putchar('\n');
+}
