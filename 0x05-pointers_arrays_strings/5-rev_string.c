afcf1c3c0b8c96f084b5da0834d7b1eac00c93ff
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-18 12:47:16 -0700

    initial code added

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,24 @@
+/**
+* rev_string - reverses a string
+* @s: string passed to the function to be reversed
+* Return: nothing
+*/
+void rev_string(char *s)
+{
+	int i, j, len;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+	char arr[len];
 
+	for (j = 0; j < len; j++)
+	{
+		arr[j] = s[len - j];
+	}
+	s = arr;
+
+}
