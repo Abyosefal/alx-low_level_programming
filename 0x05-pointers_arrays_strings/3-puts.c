a2918d623c68082711f19d336f99da1f8755fde0
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-18 12:20:58 -0700

    added initial code

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,2 +1,17 @@
+/**
+* _puts - writes a string to stdout followed by a new line
+* @str: string passed to the function to be printed
+* Return: nothing
+*/
+void _puts(char *str)
+{
+	int i;
 
-
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
