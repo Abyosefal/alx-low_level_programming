48a214fb682ba11066c2c33dadabeae18ae72806
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-19 07:22:02 -0700

    initial code

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,2 +1,10 @@
-
-
+/**
+* *_strcpy - copies string from one var to other using pointer
+* @dest: str copy to
+* @src: str original pointer
+* Return: char
+*/
+void *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+}
