37d5a45b905b087c25327c647c19e3398ac58556
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-19 04:28:16 -0700

    initial code added

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,2 +1,18 @@
+#include "main.h"
+/**
+* print_array - prints n elements of an array of int
+* @a: array passed to the function to be printed
+* @n: number of arrays to be printed
+* Return: nothing
+*/
+void print_array(int *a, int n)
+{
+	int i;
 
-
+	i = 0;
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i];
+	}
+	_putchar('\n');
+}
