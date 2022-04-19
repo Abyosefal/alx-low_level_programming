545924a67f76c4656b1ac799205b460f7f7f4238
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-19 07:50:30 -0700

    debug result

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
 * *_strcpy - copies string from one var to other using pointer
 * @dest: str copy to
@@ -9,10 +10,12 @@ void _strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
+	printf(dest);
+	printf(src);
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	/*dest[i] = '\0';*/
+	dest[i] = '\0';
 }
