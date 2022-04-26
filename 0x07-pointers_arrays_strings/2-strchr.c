ca1763b03e82189125227134f5f14996b8203b3a
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-26 00:07:05 -0700

    inital code

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr - returns the pointer to the first occurance of char c
+ * @c: char to be searched
+ * @s: string containing the char
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	j = 0;
+	while (s[j] != '\0')
+	{
+		if (s[j] == c)
+			return (s[j]);
+		j++;
+	}
+	return (NULL);
+}
