9b287a9fb8d4f1619bcd74a21ac6e6483bc2a99f
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-25 23:30:21 -0700

    initial code

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset() - fills the first n bytes of the meory pointed by s with constatn b
+ * @s: pointer to the loacation
+ * @b: the character to fill the memory
+ * @n: the int count of memory locations
+ * Return: char
+ */
+
+char *_memset(char *s. char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		*s++;
+	}
+	return (*s);
+}
