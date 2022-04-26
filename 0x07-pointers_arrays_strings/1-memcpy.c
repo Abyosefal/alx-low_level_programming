1fc6c5d097d376c9d09cd1490e34ed572403e1ac
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-25 23:56:09 -0700

    initial code

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies n bytes of the memory pointed by src to memory dest
+ * @src: pointer to the loacation
+ * @dest: the character to fill the memory
+ * @n: the int count of memory locations
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
