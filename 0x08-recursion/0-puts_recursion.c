1cab9f5d85dba50db9a391e7ba044a235dbeaa4f
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-26 22:35:21 -0700

    initial code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+	{
+		_puts(s[i]);
+		_put_recursion(s[i]);
+		i++;
+	}
+	_puts('\n');
+}
