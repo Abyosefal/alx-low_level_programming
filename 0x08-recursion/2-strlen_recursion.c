a0cf809dc4dd454009d70a769b615becc16aceda
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-27 00:31:58 -0700

    full codechange and optimization

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int i, *j;
-
-	i = 0;
-	j = &i;
-	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-		i++;	
-	}
-	return (++i);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
