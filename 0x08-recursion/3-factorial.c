dc5d5f0c077bcdd5ff1d37897cbb1ea747c2a44e
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-27 00:39:14 -0700

    initial code

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+/**
+ * factorial - returns factorial of given number
+ * @n: given number to factorial
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (0);
+	return (n * factorial(n - 1));
+}
