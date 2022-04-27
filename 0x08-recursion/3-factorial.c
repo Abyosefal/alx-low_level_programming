7c040cd9b8ade080e92b7360675ec98b794e4f2c
Besufkad <besufkadtsegaye.a@gmail.com>
2022-04-27 01:42:17 -0700

    return -1 for negative numbers

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,12 +4,12 @@
  * Return: int
  */
 
-double factorial(int n)
+int factorial(int n)
 {
 	if (n == 0)
 		return (1);
 	else if (n > 0)
 		return (n * factorial(n - 1));
 	else
-		return (n * factorial(n + 1));
+		return (-1);
 }
